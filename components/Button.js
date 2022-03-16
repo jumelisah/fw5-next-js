0d758df8a7f579c42c14b81da147f9452308bc81
juumelisa <jumelisa09@gmail.com>
2022-03-16 20:46:55 +0700

    create components button

---
+++ components/Button.js
@@ -0,0 +1,11 @@
+import styles from './Button.module.css'
+
+const Button = ({children, variant, ...rest}) => {
+  return(
+    <button className={`${styles.button} radius border-color1 py-2 ${variant} `} {...rest} >
+      {children}
+    </button>
+  )
+}
+
+export default Button
