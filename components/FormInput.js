b683bbbae2b84f2e76873dc674b39584cbfee4a4
juumelisa <jumelisa09@gmail.com>
2022-03-18 03:21:44 +0700

    edit login page

--- components/FormInput.js
+++ components/FormInput.js
@@ -1,8 +1,8 @@
 const FormInput = ({type, icon, placeholder, variant}) => {
   return(
-    <div className='position-relative'>
-    <input type={type} placeholder={placeholder} className={`px-4 py-2 ${variant}`}/>
-    <p className='position-absolute top-50 start-0 translate-middle-y'>{icon}</p>
+    <div className='position-relative mb-4'>
+    <input type={type} placeholder={placeholder} className={`px-4 py-2 ${variant}`} style={{width: '100%'}}/>
+    <p className='position-absolute top-50 start-0 translate-middle-y m-0' style={{eight: '24px'}}>{icon}</p>
     </div>
   )
 }
--- pages/login.js
+++ pages/login.js
@@ -9,21 +9,36 @@ import styles from '../styles/Login.module.css'
 
 const Login = () => {
   return(
-    <div className='row p-0 m-0'>
-      <div className={`${styles.leftSection} left-section col-7 p-0 d-none d-sm-block`}>
-        {/* <Image src='/images/bg-login.png' alt='background login' width={100} height={100} layput='responsive' sizes='100vw' /> */}
+    <div className='d-flex flex-column-reverse flex-md-row p-0 m-0'>
+      <div className={`${styles.leftSection} left-section col-12 col-md-7 px-5 d-flex align-items-center vh-md-100`}>
+        <div className='px-5'>
+        <Link href='/'>
+          <a>
+            <Image src='/images/bw.png' alt='logo' width={30} height={30} className={`${styles.logo}`}/>
+          </a>
+        </Link>
+        <div className='text-center'>
+          <Image src='/images/Group 57.png' alt='background phone' width={400} height={400} layout='fixed' priority />
+        </div>
+          <h1 className='fs-4'>App that Covering Banking Needs.</h1>
+          <p style={{maxWidth: '500px'}}>Zwallet is an application that focussing in banking needs for all users in the world. Always updated and always following world trends. 5000+ users registered in Zwallet everyday with worldwide users coverage.</p>
+        </div>
       </div>
-      <div className='right-section col-12 col-md-5 p-0 d-flex justify-content-center align-items-center vh-100'>
+      <div className='right-section col-12 col-md-5 px-3 py-5 py-md-0 d-flex justify-content-center align-items-center vh-md-100'>
         <div className='container'>
           <h1 className='fs-3 fw-bold'>Start Accessing Banking Needs With All Devices and All Platforms With 30.000+ Users</h1>
-          <p>Transfering money is eassier than ever, you can access Zwallet wherever you are. Desktop, laptop, mobile phone? we cover all of that for you!</p>
+          <p className='py-4 m-0'>Transfering money is eassier than ever, you can access BeWallet wherever you are. Desktop, laptop, mobile phone? we cover all of that for you!</p>
           <Form>
             <FormInput icon={<AiOutlineMail />} placeholder='Enter your e-mail' variant='border-0 border-bottom' />
             <FormInput icon={<FiLock />} placeholder='Enter your password' variant='border-0 border-bottom' />
-            <Link href='/'>
-              <a className='text-dark' style={{textDecoration: 'none'}}>Forgot password?</a>
-            </Link>
-            <Button variant='bg-secondary border-0'>Login</Button>
+            <div className='text-end'>
+              <Link href='/'>
+                <a className='text-dark' style={{textDecoration: 'none'}}>Forgot password?</a>
+              </Link>
+            </div>
+            <div className='my-4 p-0'>
+              <Button variant='bg-secondary border-0'>Login</Button>
+            </div>
           </Form>
             <p>Don’t have an account? Let’s 
               <Link href='/'>
