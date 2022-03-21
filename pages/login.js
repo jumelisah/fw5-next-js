76bb6ef7a88a948bd2ec6d190c2eb19a8582d018
juumelisa <jumelisa09@gmail.com>
2022-03-21 07:07:04 +0700

    edit login page

--- pages/login.js
+++ pages/login.js
@@ -13,8 +13,6 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 const Login = () => {
-  const [state, setState] = useState({email: null, password: null})
-  const handleChange = (email, password) => setState({ email, password })
   const { auth } = useSelector(state => state.auth)
   const dispatch = useDispatch()
   const route = useRouter()
@@ -57,7 +55,7 @@ const Login = () => {
           <h1 className='fs-4 fw-bold'>Start Accessing Banking Needs With All Devices and All Platforms With 30.000+ Users</h1>
           <p className='py-4 m-0'>Transfering money is eassier than ever, you can access BeWallet wherever you are. Desktop, laptop, mobile phone? we cover all of that for you!</p>
           <Form onSubmit={onLogin}>
-            <FormInput type='email' name='email' icon={<AiOutlineMail />} placeholder='Enter your e-mail' variant='border-0 border-bottom' value={state.email} onChange={handleChange}/>
+            <FormInput type='email' name='email' icon={<AiOutlineMail />} placeholder='Enter your e-mail' variant='border-0 border-bottom'/>
             <FormInput type='password' name='password' icon={<FiLock />} placeholder='Enter your password' variant='border-0 border-bottom' />
             <div className='text-end'>
               <Link href='/'>
