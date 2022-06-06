83897d6100a9a9f608b6f178903fc7cdac97535c
juumelisa <jumelisa09@gmail.com>
2022-06-06 08:29:51 +0700

    feat: 404 page

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const NotFound = () => {
+  return (
+    <div className="vh-100 vw-100 d-flex flex-column justify-content-center align-items-center m-0 p-0">
+      <Image src="/images/undraw_Page_not_found_re_e9o6.png" alt="fixing" width={400} height={400} layout="fixed" objectFit="cover"/>
+      <h1 className="text-color5">PAGE NOT FOUND</h1>
+      <Link href="/dashboard" >
+        <a className="text-color5">Back to home</a>
+      </Link>
+    </div>
+  )
+}
+export default NotFound
